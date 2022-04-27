aa742f2132fc32e114fab2b2704f8526fe495bac
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-04-27 17:43:39 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.writeln("Make your password");
+
+document.getElementById("password").innerHTML = "Scegli una password sicura";
+
+console.log(" utile per debug");
+
+// scomposizione esercizio
+let nome = prompt("Quale è il tuo nome?");
+
+let cognome = prompt("Quale è il tuo cognome?");
+
+let colore = prompt("Quale è il tuo colore preferito?");
+
+const finepsw = 21;
+
+alert = ("nome" + "cognome" + "colore" + "finepsw");
+
+console.log("test")
+
+
+
